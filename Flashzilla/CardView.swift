606e9ac4e1181580947eefd23a56f575c0ad9682
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-10-04 02:21:37 -0400

    Added a drag gesture and some animation to the app

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    var removal: (() -> Void)? = nil
     
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -27,7 +29,7 @@ struct CardView: View {
                 if isShowingAnswer {
                     Text(card.answer)
                         .font(.title)
-                    .foregroundColor(.gray)
+                        .foregroundColor(.gray)
                 }
                 
                 
@@ -36,6 +38,22 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        removal?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             isShowingAnswer.toggle()
         }
